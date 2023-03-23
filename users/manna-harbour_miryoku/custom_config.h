f832c8f8b501471f0ad8052ef60afd67ff2b6e3c
Nyfrog <128623232+Nyfrog@users.noreply.github.com>
2023-03-23 09:26:12 +0100

    Update custom_config.h

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -5,3 +5,56 @@
 
 #pragma once
 
+
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") 
+
+#define U_BUTTON U_NAV
+#define U_MEDIA U_FUN
+#define U_NUM U_NAV
+#define U_SYM U_MOUSE
+#define U_EXTRA U_NAV
+#define U_TAP U NAV
+
+
+
+#define MIRYOKU_LAYER_BASE \
+KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
+LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
+LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(U_BUTTON,KC_SLSH),\
+U_NP,              U_NP,              LT(U_FUN,KC_ESC),  LT(U_NAV,KC_SPC),  LT(U_MOUSE,KC_TAB),LT(U_FUN,KC_ENT),  LT(U_NAV,KC_BSPC), U_NP,              U_NP,              U_NP
+
+
+
+
+
+#define MIRYOKU_LAYER_FUN \
+KC_F12,            KC_F7,             KC_F8,             KC_F9,             KC_PSCR,           KC_LCTL,           RALT(KC_Y),        RALT(KC_Q),        RALT(KC_P),        RALT(KC_S),        \
+KC_F11,            KC_F4,             KC_F5,             KC_F6,             KC_SCRL,           U_NU,              KC_MPRV,           KC_VOLU,           KC_VOLD,           KC_MNXT,           \
+KC_F10,            KC_F1,             KC_F2,             KC_F3,             KC_PAUS,           OU_AUTO,           U_NU,              U_NU,              U_NU,              U_NU,              \
+U_NP,              U_NP,              KC_ESC,            KC_SPC,            KC_TAB,            KC_MUTE,           KC_DEL,            U_NP,              U_NP,              U_NP
+
+
+
+
+
+#define MIRYOKU_LAYER_NAV \
+KC_LBRC,           KC_7,              KC_8,              KC_9,              KC_RBRC,           KC_LCTL,           RALT(KC_Y),        RALT(KC_Q),        RALT(KC_P),        RALT(KC_S),        \
+KC_SCLN,           KC_4,              KC_5,              KC_6,              KC_EQL,            CW_TOGG,           KC_LEFT,           KC_UP,             KC_DOWN,           KC_RGHT,           \
+KC_GRV,            KC_1,              KC_2,              KC_3,              KC_BSLS,           KC_INS,            KC_HOME,           KC_PGUP,           KC_PGDN,           KC_END,            \
+U_NP,              U_NP,              KC_LSFT,           KC_0,              KC_MINS,           KC_LCTL,           RCTL(KC_BSPC),     U_NP,              U_NP,              U_NP	
+
+
+
+
+#define MIRYOKU_LAYER_MOUSE \
+KC_LCBR,           KC_AMPR,           KC_ASTR,           KC_LPRN,           KC_RCBR,           KC_LCTL,           RALT(KC_Y),        RALT(KC_Q),        RALT(KC_P),        RALT(KC_S),        \
+KC_COLN,           KC_DLR,            KC_PERC,           KC_CIRC,           KC_PLUS,           CW_TOGG,           KC_WH_L,           KC_WH_U,           KC_WH_D,           KC_WH_R,           \
+KC_TILD,           KC_EXLM,           KC_AT,             KC_HASH,           KC_PIPE,           U_NU,              KC_BTN3,           KC_BTN1,           KC_BTN2,           U_NU,              \
+U_NP,              U_NP,              U_NU,              KC_BTN1,           KC_BTN1,           KC_BTN2,           KC_BTN1,           U_NP,              U_NP,              U_NP
+
+
+
